f9c245117963c47b6957b82ad4a550cd0f66f242
StevenBahaa <stevenbahaa08@gmail.com>
2026-05-25 02:47:57 +0300

    refactor: split product_template.py into modular files for menu base, branch availability, and combos

--- custom_addons/restaurant_menu/models/__init__.py
+++ custom_addons/restaurant_menu/models/__init__.py
@@ -1,4 +1,6 @@
-from . import product_template
+from . import product_template_base
+from . import product_template_branch
+from . import product_template_combo
 from . import pos_category
 from . import restaurant_addon
 from . import restaurant_combo_line
