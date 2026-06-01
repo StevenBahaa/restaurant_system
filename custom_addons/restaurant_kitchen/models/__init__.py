165916533588315f320b42a1bad6487fb4f601f3
StevenBahaa <stevenbahaa08@gmail.com>
2026-06-01 19:42:17 +0300

    Add kitchen ticket model and related security and sequences.

--- custom_addons/restaurant_kitchen/models/__init__.py
+++ custom_addons/restaurant_kitchen/models/__init__.py
@@ -3,3 +3,4 @@ from . import product_kitchen_station_line
 from . import product_template
 from . import restaurant_kitchen_order
 from . import restaurant_kitchen_order_line
+from . import restaurant_kitchen_ticket
